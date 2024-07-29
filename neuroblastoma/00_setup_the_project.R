1dcc18ed8320919b9f8f2ff0e9ba276445541c68
ribaribaworking <aleksandr.bykov@ccri.at>
2024-07-29 10:32:38 +0200

    UPD cleanup old unused code

--- neuroblastoma/00_setup_the_project.R
+++ neuroblastoma/00_setup_the_project.R
@@ -2,3 +2,23 @@
 # Set up the environment, installing all the required packages
 # this might take a long time ~1h
 renv::init()
+
+
+if(!dir.exists("~/workspace/neuroblastoma/results/")) {
+  dir.create("~/workspace/neuroblastoma/results/")
+}
+
+if(!dir.exists("~/workspace/neuroblastoma/results/RNA-seq")) {
+  dir.create("~/workspace/neuroblastoma/results/RNA-seq")
+}
+
+if(!dir.exists("~/workspace/neuroblastoma/results/ATAC-seq")) {
+  dir.create("~/workspace/neuroblastoma/results/ATAC-seq")
+}
+
+
+files <- list.files(path="../Output/datasets/", pattern=".zip$")
+outDir <- "../Output/datasets/unzip"
+for (i in files) {
+  unzip(paste0("../Output/datasets/",i), exdir=outDir)
+}
