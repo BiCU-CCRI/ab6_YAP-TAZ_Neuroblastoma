dfa551be962f265c36e42005417cb0498bbd9e4a
ribaribaworking <aleksandr.bykov@ccri.at>
2025-09-11 18:07:47 +0200

    ADD SE analysis in ATAC-seq k975 data for ADR

--- neuroblastoma/resources/multiBigWigSummary.sh
+++ neuroblastoma/resources/multiBigWigSummary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-multiBigwigSummary BED-file \
---bwfiles ../data/ATACseq/bigWigs/*bigWig \
---BED ../data/ATACseq/bigWigs/CLB_SKN_M.bed \
--out ../data/ATACseq/bigWigs/scores_per_transcript.npz --outRawCounts ../data/ATACseq/bigWigs/scores_per_transcript.tab
+# multiBigwigSummary BED-file \
+# --bwfiles ../data/ATACseq/bigWigs/*bigWig \
+# --BED ../data/ATACseq/bigWigs/CLB_SKN_M.bed \
+# -out ../data/ATACseq/bigWigs/scores_per_transcript.npz --outRawCounts ../data/ATACseq/bigWigs/scores_per_transcript.tab
  
- # multiBigwigSummary BED-file \
- # --bwfiles ../data/ATACseq/bigWigs/*bigWig \
- # --BED ../data/ATACseq/bigWigs/CLB_SKN_M_h3K27Ac.bed \
- # -out ../data/ATACseq/bigWigs/scores_per_transcript_clbm_skn_MES_SEs_h3k27ac.npz \
- # --outRawCounts ../data/ATACseq/bigWigs/scores_per_transcript_clbm_skn_MES_SEs_h3k27ac.tab
+ multiBigwigSummary BED-file \
+ --bwfiles ../data/ATACseq/bigWigs/*bigWig \
+ --BED ../data/ATACseq/bigWigs/CLB_SKN_A.bed \
+ -out ../data/ATACseq/bigWigs/scores_per_transcript_clbm_skn_ADR.npz \
+ --outRawCounts ../data/ATACseq/bigWigs/scores_per_transcript_clbm_skn_ADR.tab
